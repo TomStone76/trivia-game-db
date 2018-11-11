fb63e75d6e9728a12c27d09dd768c237867f0e4b
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-11 17:31:31 -0500

    commit

--- schema.sql
+++ schema.sql
@@ -3,5 +3,8 @@ CREATE DATABASE trivia_db;
 USE trivia_db;
 
 CREATE TABLE questions (
-    
+    id INTEGER AUTO_INCREMENT,
+    question VARCHAR(100) NOT NULL,
+    answer BOOLEAN NOT NULL,
+    PRIMARY KEY id
 );
