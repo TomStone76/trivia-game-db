780bfd672d7125c00e165a4e1bcd04fb668a7075
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-11 20:53:38 -0500

    commit

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO questions(question, answer)
+VALUES ("Paris is the capital of France.", true);
+
+INSERT INTO questions(question, answer)
+VALUES ("The Eagles won the Superbowl in 2018.", true);
+
+INSERT INTO questions(question, answer)
+VALUES ("Cats are canines.", false);
+
+INSERT INTO questions(question, answer)
+VALUES ("Dogs are felines.", false);
+
+INSERT INTO questions(question, answer)
+VALUES ("China has the largest population of any country.", true);
